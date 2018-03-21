44e93ce1c5f6c9a939de4890bb5a82d1b88107c5
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-03-21 16:52:34 -0700

    make it simpler

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express                 = require('express'),
-      path                    = require('path'),
-      mongoose                = require("mongoose"),
-      passport                = require("passport"),
-      flash                   = require('express-flash'),
-      methodOverride          = require("method-override"),
-      bodyParser              = require('body-parser'),
-      localStrategy           = require("passport-local"),
-      User                    = require("./models/user");
+      mongoose                = require("mongoose");
 
 /**
  * Controllers (route handlers).
  */
-// Home route to handle Home Page and other pages (if coming up in the future
-// const homeRoute = require('./routes/home');
-// User route to handle Login Logout Signup routes
-// const userRoute = require('./routes/user');
 const handlers = require('./util/route_handlers.js');
 
 /**
@@ -32,20 +21,7 @@ mongoose.connect(process.env.DATABASEURL || "mongodb://localhost/CMPEit", {});
  * Express configuration.
  */
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true})) ;
-app.use(methodOverride("_method"));
 app.use(express.static('public'));
-app.use(passport.initialize());
-app.use(passport.session());
-// app.use(flash());
-
-/**
- * Uncomment to use to handle User Authentication
- */
-passport.use(new localStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 /**
  * Primary app routes.
@@ -63,18 +39,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-/**
- * error handler
- */
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 /**
  * Start Express server.
