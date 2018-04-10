8128cef74bb9ceb0ee93e0dd2eb570d683d8990a
Aris Koumis <ariskoumis@gmail.com>
2018-04-10 11:47:54 -0700

    Abstracted initialization of database. mongodb processes now sit in ./util/database.js

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 var express = require('express');
-var mongo = require('mongodb');
 var app = express();
 
-/**
- * Import Controllers (route handlers for enpoints).
- */
+var database = require('./util/database.js');
 var handlers = require('./util/route_handlers.js');
 
 /**
  * Connect to MongoDB.
  */
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+database.init();
 
 /**
  * Express configuration.
