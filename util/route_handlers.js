28dcba3abc181d2a452d43d5e5759c0046bf03e2
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-04-11 21:05:48 -0700

    alert doesn't appear

--- util/route_handlers.js
+++ util/route_handlers.js
@@ -1,94 +1,77 @@
-/**
- * Module dependencies.
- */
-var EventEmitter 		= require('events'),
-		path 						= require('path'),
-		database 				= require('./database.js'),
-		Stream 					= new EventEmitter(),
-  	handler_map 		= {};
+var EventEmitter = require('events');
 
-/**
- * Get HomePage
- */
+var handler_map = {};
+var path = require('path');
+var database = require('./database.js');
+const Stream = new EventEmitter();
+
 handler_map.rootHandler = function (req, res) {
-	res.set("Content-Type", "text/html");
-	res.sendFile(path.resolve(__dirname + '/../public/index.html'), function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("we good.")
-		}
-	});
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.resolve(__dirname + '/../public/index.html'), function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("we good.")
+    }
+  });
 };
 
-/**
- * Login Function
- */
-handler_map.attemptLoginHandler = function (req) {
+handler_map.attemptLoginHandler = function (req, res) {
   var data = req.body;
-  if (data.username === "" | data.password === "") {
-    console.log("You're missing one section, please fill all to login.");
-    Stream.emit("push", "message", {event: "login_result", result: false});;
-  } else {
-    database.mongoclient.connect(database.url, function (err, client) {
-      if (err) throw err;
-      var db = client.db("mydb");
-      db.collection("users").findOne({username: data.username}, function (err, res) {
-        if (res !== null && res.password === data.password) {
-          console.log("User Does Exist, Loggin successfully");
-          Stream.emit("push", "message", {event: "login_result", result: true});
-        } else {
-          console.log("Please enter a correct password");
-          Stream.emit("push", "message", {event: "login_result", result: false});
-        }
-        client.close();
-      })
-    });
-  }
+  console.log("attempt login");
+
+  database.mongoclient.connect(database.url, function(err, client) {
+    if (err) throw err;
+    var db = client.db("mydb");
+    db.collection("users").findOne({username: data.username}, function (err, res) {
+      if (res !== null && res.password === data.password) {
+        console.log("User Does Exist, Loggin successfully");
+        Stream.emit("push", "message", { event: "login_result", result: true});
+      } else {
+        console.log("Please enter a correct password");
+        Stream.emit("push", "message", { event: "login_result", result: false});
+      }
+      client.close();
+    })
+  });
 };
 
-/**
- * Signup Function
- */
 handler_map.createAccountHandler = function (req, res) {
   var data = req.body;
-  // database.signup("users", data);
-  if (data.username === "" | data.password === "" | data.email === "") {
-    console.log("You're missing one section, please fill all to signup.");
-    Stream.emit("push", "message", {event: "create_account_result", result: false});
-  } else {
-    //Write to data to collection titled 'users'
-    database.mongoclient.connect(database.url, function (err, client) {
-      if (err) throw err;
-      var db = client.db("mydb");
-      db.collection("users").findOne({username: data.username}, function (err, res) {
-        if (res !== null) {
-          console.log("User does Exist, please enter a different username");
-          Stream.emit("push", "message", {event: "create_account_result", result: false});
-        } else {
-          console.log("Congratulation, you just create an account");
-          db.collection("users").insertOne(data);
-          Stream.emit("push", "message", {event: "create_account_result", result: true});
-        }
-        client.close();
-      })
-    });
-  }
+
+  //Write to data to collection titled 'users'
+  database.mongoclient.connect(database.url, function (err, client) {
+    if (err) throw err;
+    var db = client.db("mydb");
+    db.collection("users").findOne({ username: data.username }, function (err, res) {
+      if (res !== null) {
+        console.log("User does Exist, please enter a different username");
+        Stream.emit("push", "message", { event: "create_account_result", result: false });
+      } else {
+        console.log("Congratulation, you just create an account");
+        db.collection("users").insertOne(data);
+        Stream.emit("push", "message", { event: "create_account_result", result: true });
+      }
+      client.close();
+    })
+  });
+
+
+  // var success = database.write("users", data);
+  // console.log("This is the line: " + success);
 };
 
-/**
- * Initialize SSE Handler
- */
 handler_map.initializeSSEHandler = function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/event-stream',
-		'Cache-Control': 'no-cache',
-		'Connection': 'keep-alive'
-	});
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
 
-	Stream.on("push", function (event, data) {
-		res.write("event: " + String(event) + "\n" + "data: " + JSON.stringify(data) + "\n\n");
-	});
+  Stream.on("push", function (event, data) {
+    res.write("event: " + String(event) + "\n" + "data: " + JSON.stringify(data) + "\n\n");
+  });
 };
 
+//export
 module.exports = handler_map;
