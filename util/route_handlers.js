bc654fc89ea8f8b59ca36b3710f0282b0c11e333
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-04-15 19:33:26 -0700

    Create a Function where a user can create a post

--- util/route_handlers.js
+++ util/route_handlers.js
@@ -3,10 +3,14 @@
  * Module dependencies.
  */
 var EventEmitter 		= require('events'),
-  path 						= require('path'),
-  database 				= require('./database.js'),
-  Stream 					= new EventEmitter(),
-  handler_map 		= {};
+    path 						= require('path'),
+    database 				= require('./database.js'),
+    Stream 					= new EventEmitter(),
+    handler_map 		= {},
+    // Add currentUser to know which user is in the system currently
+    currentUser     = {},
+  // List of Post. Each Post will have a title, content, and author, later on will have comment too, but not right now
+    Post            = {};
 
 /**
  * Get HomePage
@@ -27,7 +31,8 @@ handler_map.rootHandler = function (req, res) {
  */
 handler_map.attemptLoginHandler = function (req) {
   var data = req.body;
-  if (data.username === "" | data.password === "") {
+
+  if (data.username === "" || data.password === "") {
     console.log("You're missing one section, please fill all to login.");
     Stream.emit("push", "message", {event: "login_result", result: false});;
   } else {
@@ -36,14 +41,21 @@ handler_map.attemptLoginHandler = function (req) {
       var db = client.db("mydb");
       db.collection("users").findOne({username: data.username}, function (err, res) {
         if (res !== null && res.password === data.password) {
-          console.log("User Does Exist, Loggin successfully");
+          console.log("User Does Exist, Login successfully ");
+          currentUser = {
+            id: res._id,
+            username: res.username,
+            password: res.password
+          };
           Stream.emit("push", "message", {event: "login_result", result: true});
         } else {
           console.log("Please enter a correct password");
           Stream.emit("push", "message", {event: "login_result", result: false});
         }
-        client.close();
-      })
+        console.log(currentUser);
+        // console.log("Current User is: " + currentUser.username);
+      });
+      client.close();
     });
   }
 };
@@ -54,7 +66,7 @@ handler_map.attemptLoginHandler = function (req) {
 handler_map.createAccountHandler = function (req, res) {
   var data = req.body;
   // database.signup("users", data);
-  if (data.username === "" | data.password === "" | data.email === "") {
+  if (data.username === "" || data.password === "" || data.email === "") {
     console.log("You're missing one section, please fill all to signup.");
     Stream.emit("push", "message", {event: "create_account_result", result: false});
   } else {
@@ -75,7 +87,42 @@ handler_map.createAccountHandler = function (req, res) {
       })
     });
   }
+  // console.log(req.user);
 };
+
+/**
+ * Create A Post Function
+ */
+handler_map.createAPostHandler = function (req, res) {
+  var data = req.body;
+
+  // Information of the Post
+  var name            = data.name,
+      desc            = data.description;
+
+  // Information of the user
+  var author          = {
+    id: currentUser.id,
+    username: currentUser.username
+  };
+
+  var newPost   = {
+    name: name,
+    description: desc,
+    author: author
+  };
+
+  // Create a new Post and save to the database
+  Post.push(newPost, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Need Aris to create a route that lead to the Homepage. HomePage may contains bunch of Posts.
+      res.redirect("/");
+    }
+  });
+};
+
 
 /**
  * Initialize SSE Handler
