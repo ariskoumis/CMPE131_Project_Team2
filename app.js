905a5627c636c37af37dd8d7aa24c47236674189
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-04-16 21:59:26 -0700

    try passport but need to use mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 /**
  * Module dependencies.
  */
-var express       = require('express'),
-    bodyParser    = require('body-parser');
+var express                 = require('express'),
+    bodyParser              = require('body-parser'),
+    passport                = require("passport"),
+    User                    = require("./models/user.js"),
+    localStrategy           = require("passport-local");
 
 /**
  * Route Handler
@@ -32,10 +35,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function(req, res, next){
-  res.locals.currentUser  = currUser;
-  next();
-});
+app.use(passport.initialize());
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 /**
  * Primary app routes.
@@ -44,6 +58,7 @@ app.get('/', homeRoute.rootHandler);
 app.get('/stream', homeRoute.initializeSSEHandler);
 app.post('/attempt-login', homeRoute.attemptLoginHandler);
 app.post('/create-account', homeRoute.createAccountHandler);
+// app.post('/create-account', homeRoute.register);
 
 app.get('/new-post', postRoute.getPostForm);
 app.get('/show-post', postRoute.showPost);
--- util/index.js
+++ util/index.js
@@ -2,14 +2,16 @@
  * Module dependencies.
  */
 var EventEmitter 		= require('events'),
-  path 						= require('path'),
-  database 				= require('./database.js'),
-  Stream 					= new EventEmitter(),
-  handler_map 		= {
-    currentUser: {
-      existed: false
-    }
-  };
+    path 						= require('path'),
+    database 				= require('./database.js'),
+    Stream 					= new EventEmitter(),
+    passport        = require("passport"),
+    User            = require("../models/user"),
+    handler_map 		= {
+      currentUser: {
+        existed: false
+      }
+    };
 
 
 handler_map.getCurrentUser = function() {
@@ -24,70 +26,101 @@ handler_map.rootHandler = function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../public/index.html'));
 };
 
+
+
+
 /**
  * Login Function
  */
 handler_map.attemptLoginHandler = function (req) {
-  var data = req.body;
-  if (handler_map.currentUser.existed === false) {
-    if (data.username === "" || data.password === "") {
-      Stream.emit("push", "message", {event: "login_result", result: false, message: "You're missing one section, please fill all to login."});
-    } else {
-      database.mongoclient.connect(database.url, function (err, client) {
-        if (err) throw err;
-        var db = client.db("mydb");
-        db.collection("users").findOne({username: data.username}, function (err, res) {
-          if (res !== null && res.password === data.password) {
-            console.log("User Does Exist, Login successfully ");
-            handler_map.currentUser = {
-              id: res._id,
-              username: res.username,
-              password: res.password,
-              existed: true
-            };
-            console.log(handler_map.currentUser);
-            Stream.emit("push", "message", {event: "login_result", result: true});
-          } else {
-            console.log("Please enter a correct password");
-            Stream.emit("push", "message", {event: "login_result", result: false});
-          }
-        });
-        client.close();
-      });
+  console.log("checking");
+  passport.authenticate('local'),
+    function (req, res) {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      // res.redirect('/users/' + req.user.username);
+      console.log("success");
     }
-  } else {
-    console.log("You're already logged in!");
-  }
 };
+
+
+
+
+  // var data = req.body;
+  // if (handler_map.currentUser.existed === false) {
+  //   if (data.username === "" || data.password === "") {
+  //     Stream.emit("push", "message", {event: "login_result", result: false, message: "You're missing one section, please fill all to login."});
+  //   } else {
+  //     database.mongoclient.connect(database.url, function (err, client) {
+  //       if (err) throw err;
+  //       var db = client.db("mydb");
+  //       db.collection("users").findOne({username: data.username}, function (err, res) {
+  //         if (res !== null && res.password === data.password) {
+  //           console.log("User Does Exist, Login successfully ");
+  //           handler_map.currentUser = {
+  //             id: res._id,
+  //             username: res.username,
+  //             password: res.password,
+  //             existed: true
+  //           };
+  //           console.log(handler_map.currentUser);
+  //           Stream.emit("push", "message", {event: "login_result", result: true});
+  //         } else {
+  //           console.log("Please enter a correct password");
+  //           Stream.emit("push", "message", {event: "login_result", result: false});
+  //         }
+  //       });
+  //       client.close();
+  //     });
+  //   }
+  // } else {
+  //   console.log("You're already logged in!");
+  // }
+// };
 
 /**
  * Signup Function
  */
 handler_map.createAccountHandler = function (req) {
-  var data = req.body;
-  // database.signup("users", data);
-  if (data.username === "" || data.password === "" || data.email === "") {
-    console.log("You're missing one section, please fill all to signup.");
-    Stream.emit("push", "message", {event: "create_account_result", result: false});
-  } else {
-    //Write to data to collection titled 'users'
-    database.mongoclient.connect(database.url, function (err, client) {
-      if (err) throw err;
-      var db = client.db("mydb");
-      db.collection("users").findOne({username: data.username}, function (err, res) {
-        if (res !== null) {
-          console.log("User does Exist, please enter a different username");
-          Stream.emit("push", "message", {event: "create_account_result", result: false});
-        } else {
-          console.log("Congratulation, you just create an account");
-          db.collection("users").insertOne(data);
-          Stream.emit("push", "message", {event: "create_account_result", result: true});
-        }
-        client.close();
+
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    console.log("tesing");
+    if(err) {
+      console.log("Fail register");
+      // req.flash("error", err.message);
+      // return res.render("register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        console.log("success register");
+        // req.flash("success", "Welcome to YelpCamp " + user.username);
+        // res.redirect("/campgrounds");
       })
-    });
-  }
-  // console.log(req.user);
+    }
+  });
+  // var data = req.body;
+  // // database.signup("users", data);
+  // if (data.username === "" || data.password === "" || data.email === "") {
+  //   console.log("You're missing one section, please fill all to signup.");
+  //   Stream.emit("push", "message", {event: "create_account_result", result: false});
+  // } else {
+  //   //Write to data to collection titled 'users'
+  //   database.mongoclient.connect(database.url, function (err, client) {
+  //     if (err) throw err;
+  //     var db = client.db("mydb");
+  //     db.collection("users").findOne({username: data.username}, function (err, res) {
+  //       if (res !== null) {
+  //         console.log("User does Exist, please enter a different username");
+  //         Stream.emit("push", "message", {event: "create_account_result", result: false});
+  //       } else {
+  //         console.log("Congratulation, you just create an account");
+  //         db.collection("users").insertOne(data);
+  //         Stream.emit("push", "message", {event: "create_account_result", result: true});
+  //       }
+  //       client.close();
+  //     })
+  //   });
+  // }
+  // // console.log(req.user);
 };
 
 /**
