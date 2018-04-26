691da29be8c08ebc42b4b12f0958baa4c67f5f1e
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-04-25 17:53:35 -0700

    fix Aris Code

--- routes/comment.js
+++ routes/comment.js
@@ -48,13 +48,13 @@ handler_map.createNewComment = function(req, res) {
     if (err) throw err;
     var db = client.db("cmpe-it");
     db.collection("posts").findOne({_id: req.params.id}, function (err, foundPost) {
+      console.log(req.params.id);
+      console.log(foundPost);
       if (err) throw err;
       else {
-        console.log("go here 1");
         db.collection("comments").insertOne(newComment, function (err, comment) {
           if (err) throw err;
           else {
-            console.log("go here 2");
             res.redirect('/post/show-post');
             foundPost.comment.push(comment);
           }
