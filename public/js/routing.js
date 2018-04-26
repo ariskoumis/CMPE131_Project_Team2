4c7d6323f4f3d54665336e0d245d21dc1954542f
Calvin Nguyen <cuongvnguyen96@berkeley.edu>
2018-04-25 17:19:26 -0700

    cmpe-it2

--- public/js/routing.js
+++ public/js/routing.js
@@ -19,7 +19,7 @@ export default routing;
  */
 routing.sendPostRequest = function (endpoint, data) {
   var xhr = new XMLHttpRequest();
-  var url = "http://localhost:8000/" || "cmpe-it1.herokuapp.com";
+  var url = "http://localhost:8000/" || "cmpe-it2.herokuapp.com";
   xhr.open("POST", url + endpoint, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
 
